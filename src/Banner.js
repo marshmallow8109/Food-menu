f1d3a24eed7722b2fbb10cca1d0200e0f3f682d2
Austine <augustrush.ae@gmail.com>
2023-02-14 13:12:22 +0100

    tagline added

--- src/Banner.js
+++ src/Banner.js
@@ -4,6 +4,7 @@ const Banner = () => {
       <section className="nav-center">
         <div className="Hero">
           <h1>Store Name</h1>
+          <p className="tagline">add.yourtagline.here</p>
         </div>
       </section>
     </>
